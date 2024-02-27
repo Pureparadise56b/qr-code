e22ee0db41a1a98f1e30fc67bbefe0b45df0d0b9
PureParadise56b <toufique26ajay@gmail.com>
2024-02-27 23:35:07 +0530

    bug fixes

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
     <input
       type={type}
       className={cn(
-        'flex h-9 w-[25vw] rounded-md border border-input bg-white px-3 py-2 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 caret-zinc-600 text-white',
+        'flex h-9 w-[25vw] rounded-md border border-input bg-white px-3 py-2 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 caret-zinc-600',
         className
       )}
       ref={ref}
